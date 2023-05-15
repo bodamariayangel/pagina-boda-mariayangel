9259471a6206eae53b4fa32fca7ff038b742ad79
bodamariayangel <bodamariayangel0508@gmail.com>
2023-05-15 21:24:34 +0200

    fix negative form and video yt

--- js/invite.js
+++ js/invite.js
@@ -48,6 +48,61 @@ Invite = {
         });
     },
     
+    // $('body').on('click', '#sendAsistencia', function(e) {
+        //   e.preventDefault();
+        //   if (isOkAsistencia()) //Form bien validado
+        //   {
+        //     // Load and disabled buttom.
+        //     $("#sendAsistencia").text(lang_informandoAsistencia + "...");
+        //     $("#sendAsistencia").prop("disabled", true);
+        //     // Obtengo el Form.
+        //     var formulario = $("#formAsistencia")[0];
+        //     // Obtengo los datos del formulario
+        //     var datos = new FormData(formulario);
+        //     // Visualizar como viajan los datos
+        //     // for (var pair of datos.entries()) {
+        //     //   console.log(pair[0] + ", " + pair[1]);
+        //     // }
+        //     // Envio con fetch los datos mediante POST
+        //     fetch(_pathApp + "producto/fetchs/confirmar-asistencia.php", {
+        //         method: "POST",
+        //         body: datos
+        //       })
+        //       // Promesas fetch
+        //       .then(res => res.json())
+        //       .then(data => {
+        //         // Si recibo error 
+        //         if (data.error === true) {
+        //           $("#sendAsistencia").text(lang_confirmarAsistencia);
+        //           $("#sendAsistencia").prop("disabled", false);
+        //           // Imprimo el error
+        //           $('#formAsistencia').after('<span id="error-form">' + data.desc + '</span>');
+        //         }
+        //         // Si no hay error 
+        //         if (data.error === false) {
+        //           $("#sendAsistencia").text(lang_confirmarAsistencia);
+        //           $("#sendAsistencia").prop("disabled", false);
+        //           // Oculto elementos del form y reseteo
+        //           $('#formAsistencia')[0].reset();
+        //           $('#modalAsistencia .formulario-content, #modalAsistencia .modal-footer, #modalAsistencia h5').hide();
+        //           // Acomodo el css para centrar mensaje
+        //           $('#modalAsistencia .modal-body').addClass('fix-height');
+        //           // Muestro mensaje de exito
+        //           $('#modalAsistencia .msj-content').html(
+        //             "<h5>" + lang_asistenciaMsjExito_1 + "</h5><p>" + lang_asistenciaMsjExito_2 + "</p>"
+        //           ).show();
+        //           // Cierro modal y vuelvo a activar form
+        //           setTimeout(function() {
+        //             $('#modalAsistencia').modal('hide');
+        //             $('#modalAsistencia .formulario-content, #modalAsistencia .modal-footer, #modalAsistencia h5').show();
+        //             $('#modalAsistencia .msj-content').html('').hide();
+        //             $('#modalAsistencia .modal-body').removeClass('fix-height');
+        //           }, 4000);
+        //         }
+        //       });
+        //   }
+        // });
+
     parallax: function(){
         if (device == 'mobile' || $(window).width() < 768) {
             var portadaParallax = './img/portada-mobile.jpg';
